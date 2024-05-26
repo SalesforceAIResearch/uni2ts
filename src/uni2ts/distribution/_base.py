fix masking of MultiOutSizeLinear when dimension > 1

src/uni2ts/distribution/_base.py
@@ -77,7 +77,10 @@ class DistrParamProj(nn.Module):
                     proj_layer(in_features, dim * out_features, **kwargs)
                     if isinstance(out_features, int)
                     else proj_layer(
-                        in_features, tuple(dim * of for of in out_features), **kwargs
+                        in_features,
+                        tuple(dim * of for of in out_features),
+                        dim,
+                        **kwargs,
                     )
                 ),
                 args_dim,