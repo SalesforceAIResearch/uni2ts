78dd5fecbb72f1cb9f288c48cf02e9b919e032c9
chenmouxiang <cmx_1007@foxmail.com>
2024-09-13 08:22:22 +0000

    Update VisionTS evaluation scripts

--- project/benchmarks/run_visionts.py
+++ project/benchmarks/run_visionts.py
@@ -7,6 +7,7 @@ warnings.filterwarnings('ignore')
 import numpy as np
 import einops
 from visionts import VisionTS
+import pandas as pd
 import torch
 from gluonts.ev.metrics import (
     MAE,
@@ -44,18 +45,78 @@ def imputation_nan(array):
     return array_imputed
 
 
+POSSIBLE_SEASONALITIES = {
+    "S": [3600],  # 1 hour
+    "T": [1440, 10080],  # 1 day or 1 week
+    "H": [24, 168],  # 1 day or 1 week
+    "D": [7, 30, 365],  # 1 week, 1 month or 1 year
+    "W": [52, 4], # 1 year or 1 month
+    "M": [12, 6, 3], # 3 months, 6 months or 1 year
+    "B": [5],
+    "Q": [4, 2], # 6 months or 1 year
+}
+
+def norm_freq_str(freq_str: str) -> str:
+    base_freq = freq_str.split("-")[0]
+    if len(base_freq) >= 2 and base_freq.endswith("S"):
+        return base_freq[:-1]
+    return base_freq
+
+def get_seasonality_list(freq: str) -> int:
+    offset = pd.tseries.frequencies.to_offset(freq)
+    base_seasonality_list = POSSIBLE_SEASONALITIES.get(norm_freq_str(offset.name), 1)
+    seasonality_list = []
+    for base_seasonality in base_seasonality_list:
+        seasonality, remainder = divmod(base_seasonality, offset.n)
+        if not remainder:
+            seasonality_list.append(seasonality)
+    seasonality_list.append(1)
+    return seasonality_list
+
+def forcast_batch(batch, device, context_len, align_const, model: VisionTS):
+    # input batch: list of [L, ].
+    # return: [B, H]
+    context = [
+        torch.tensor(
+            imputation_nan(entry[-context_len:])
+        ).view((1, -1, 1)).to(device)
+        for entry in batch
+    ]
+    try:
+        context_list = [torch.concatenate(context, dim=0)]
+    except RuntimeError:
+        # Context lengths are not the same. Should process one by one.
+        context_list = context
+    cur_forecast_samples = []
+    for cur_context in context_list:
+        real_context_length = cur_context.shape[1]
+        if real_context_length != model.context_len:
+            model.update_config(real_context_length, model.pred_len, model.periodicity, model.norm_const, align_const)
+        forecasts = model.forward(cur_context, fp64=True)
+        forecasts = einops.rearrange(forecasts, 'b t 1 -> b t').detach().cpu().numpy()
+        cur_forecast_samples.append(forecasts)
+    return np.concatenate(cur_forecast_samples, axis=0)
+
+def convert_context_len(context_len, no_periodicity_context_len, periodicity):
+    if periodicity == 1:
+        context_len = no_periodicity_context_len
+    # Round context length to the integer multiples of the period
+    context_len = int(round(context_len / periodicity)) * periodicity
+    return context_len
+
 def evaluate(
     model,
     dataset,
     save_path,
     context_len,
+    no_periodicity_context_len,
     device='cuda:0',
     checkpoint_dir="./ckpt",
     mae_arch='mae_base',
     batch_size=512,
     test_setting="monash",
     prediction_length=96,
-    periodicity=1,
+    periodicity='autotune',
     norm_const=0.4,
     align_const=0.4,
 ):
@@ -81,35 +142,49 @@ def evaluate(
             if model == "visionts":
                 # Load VisionTS
                 model = VisionTS(mae_arch, ckpt_dir=checkpoint_dir).to(device)
-                # Round context length to the integer multiples of the period
-                context_len = int(round(context_len / periodicity)) * periodicity
-                model.update_config(context_len, prediction_length, periodicity, norm_const, align_const)
-                print(f"Model - VisionTS loaded with batch_size: {batch_size}, dataset = {dataset}, periodicity = {periodicity}, context_len = {context_len}, pred_len = {prediction_length}")
+                print(f"Model - VisionTS loaded with batch_size: {batch_size}, dataset = {dataset}, pred_len = {prediction_length}")
             else:
                 raise NotImplementedError(f"Model {model} not implemented")
+            
+            if periodicity == 'autotune':
+                # Validate seasonality list
+                seasonality_list = get_seasonality_list(metadata.freq)
+                best_valid_mae = float('inf')
+                best_valid_p = 1
+                for periodicity in seasonality_list:
+                    # Round context length to the integer multiples of the period
+                    context_len = convert_context_len(context_len, no_periodicity_context_len, periodicity)
+                    model.update_config(context_len, prediction_length, periodicity, norm_const, align_const)
+                    cur_mae_list = []
+                    for batch in tqdm(list(batcher(test_data.input, batch_size=batch_size)), desc=f"Validate periodicity: {periodicity}"):
+                        input_batch = [x['target'][:-prediction_length] for x in batch]
+                        label_batch = np.stack([x['target'][-prediction_length:] for x in batch])
+                        cur_forecast_samples = forcast_batch(input_batch, device, context_len, align_const, model)
+                        assert cur_forecast_samples.shape == label_batch.shape
+                        cur_mae_list.append(np.abs(label_batch - cur_forecast_samples))
+                    cur_valid_mae = np.mean(np.concatenate(cur_mae_list, axis=0))
+                    if cur_valid_mae < best_valid_mae:
+                        best_valid_p = periodicity
+                        best_valid_mae = cur_valid_mae
+                        print(f"autotune: P = {periodicity} | valid mae = {cur_valid_mae}, accept!")
+                    else:
+                        print(f"autotune: P = {periodicity} | valid mae = {cur_valid_mae}, reject!")
+                periodicity = best_valid_p
+            elif periodicity == 'freq':
+                periodicity = get_seasonality_list(metadata.freq)[0]
+            else:
+                periodicity = int(periodicity)
+
+            print(f"Use periodicity = {periodicity}, context len = {context_len}")
+
             # Generate forecast samples
             forecast_samples = []
+            context_len = convert_context_len(context_len, no_periodicity_context_len, periodicity)
+            model.update_config(context_len, prediction_length, periodicity, norm_const, align_const)
             for batch in tqdm(list(batcher(test_data.input, batch_size=batch_size)), desc="Forecasting"):
-                context = [
-                    torch.tensor(
-                        imputation_nan(entry["target"][-context_len:])
-                    ).view((1, -1, 1)).to(device)
-                    for entry in batch
-                ]
-                try:
-                    context_list = [torch.concatenate(context, dim=0)]
-                except RuntimeError:
-                    # Context lengths are not the same. Should process one by one.
-                    context_list = context
-                cur_forecast_samples = []
-                for cur_context in context_list:
-                    real_context_length = cur_context.shape[1]
-                    if real_context_length != model.context_len:
-                        model.update_config(real_context_length, prediction_length, periodicity, norm_const, align_const)
-                    forecasts = model.forward(cur_context, fp64=True)
-                    forecasts = einops.rearrange(forecasts, 'b t 1 -> b t').detach().cpu().numpy()
-                    cur_forecast_samples.append(forecasts)
-                forecast_samples.append(np.concatenate(cur_forecast_samples, axis=0))
+                batch = [x['target'] for x in batch]
+                cur_forecast_samples = forcast_batch(batch, device, context_len, align_const, model)
+                forecast_samples.append(cur_forecast_samples)
             forecast_samples = np.concatenate(forecast_samples, axis=0)
             break
         except torch.cuda.OutOfMemoryError:
@@ -158,7 +233,12 @@ if __name__ == "__main__":
         "--dataset", type=str, required=True, help="Name of the dataset to use"
     )
     parser.add_argument(
-        "--periodicity", type=int, required=True, help="Time series periodicity length. If unknown, you can use 1. "
+        "--periodicity", type=str, required=True, 
+        help=("Time series periodicity length. Can be the following param: " +
+        "(1) 'autotune': find the best periodicity on the validation set based on frequency " +
+        "(2) 'freq': use the pre-defined periodicity based on frequency " +
+        "(3) An integer: use the given periodicity."
+        )
     )
     parser.add_argument(
         "--save_dir", type=str, default="results", help="Directory to save the results"
@@ -171,6 +251,12 @@ if __name__ == "__main__":
         type=int, 
         default=1000, 
         help="Context length."
+    )
+    parser.add_argument(
+        "--no_periodicity_context_len", 
+        type=int, 
+        default=1000, 
+        help="Context length for data with periodicity = 1."
     )
     parser.add_argument(
         "--batch_size", type=int, default=512, help="Batch size for generating samples"
@@ -220,6 +306,7 @@ if __name__ == "__main__":
             args.dataset,
             save_dir,
             context_len=args.context_len,
+            no_periodicity_context_len=args.no_periodicity_context_len,
             device=args.device,
             checkpoint_dir=args.checkpoint_dir,
             batch_size=args.batch_size,
--- project/benchmarks/visionts_scripts/lsf_visionts.sh
+++ project/benchmarks/visionts_scripts/lsf_visionts.sh
@@ -1,10 +1,23 @@
-#!/bin/bash
+for pl in 96 192 336 720; do
+  python run_visionts.py --dataset=ETTh1  --run_name=visionts --save_dir=lsf_results --test_setting=lsf --model=visionts --periodicity=freq --context_len=2880 --pred_length=${pl} --batch_size 512
+done
 
-DATASET=visionts_scripts/lsf_ds.txt
+for pl in 96 192 336 720; do
+  python run_visionts.py --dataset=ETTh2  --run_name=visionts --save_dir=lsf_results --test_setting=lsf --model=visionts --periodicity=freq --context_len=1728 --pred_length=${pl} --batch_size 512
+done
 
-while read -r ds P L; do
-  for pl in 96 192 332 720;
-  do
-    python run_visionts.py --dataset=${ds}  --run_name=visionts --save_dir=lsf_results --test_setting=lsf --model=visionts --periodicity=${P} --context_len=${L} --pred_length=${pl} --batch_size 512
-  done  
-done < ${DATASET}
+for pl in 96 192 336 720; do
+  python run_visionts.py --dataset=ETTm1  --run_name=visionts --save_dir=lsf_results --test_setting=lsf --model=visionts --periodicity=freq --context_len=2304 --pred_length=${pl} --batch_size 512
+done
+
+for pl in 96 192 336 720; do
+  python run_visionts.py --dataset=ETTm2  --run_name=visionts --save_dir=lsf_results --test_setting=lsf --model=visionts --periodicity=freq --context_len=4032 --pred_length=${pl} --batch_size 512
+done
+
+for pl in 96 192 336 720; do
+  python run_visionts.py --dataset=electricity  --run_name=visionts --save_dir=lsf_results --test_setting=lsf --model=visionts --periodicity=freq --context_len=2880 --pred_length=${pl} --batch_size 512
+done
+
+for pl in 96 192 336 720; do
+  python run_visionts.py --dataset=weather  --run_name=visionts --save_dir=lsf_results --test_setting=lsf --model=visionts --periodicity=freq --context_len=4032 --pred_length=${pl} --batch_size 512
+done
